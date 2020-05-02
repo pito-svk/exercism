9fb595041894370df5f81a3bcb41854d27f6cb50
Peter Parada <parad.peter@gmail.com>
2020-05-02 12:59:51 +0200

    Indent and remove/update comments

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,13 +1,11 @@
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package gigasecond
 
 import (
-	"time"
 	"math"
+	"time"
 )
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond returns time gigasecond after input time
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(time.Second * time.Duration(math.Pow(10, 9)))
 }
